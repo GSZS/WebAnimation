e9db7ebb12d1fb1b8a962a25d519bd04f35cfa34
幸焕光 <xhg1945454120@gmail.com>
2020-09-21 21:24:30 +0800

    feat(迭代动画): 增加FLIP动画示例-多维网格

---
+++ js/flip.js
@@ -0,0 +1,36 @@
+let SISTERS = [], initLength = 100;
+for (let i = 1; i <= initLength; i++) { 
+  SISTERS.push(i)
+}
+
+let initialSize = 81
+let randomSize = [1, 2, 3, 4]
+
+let getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// by ustbhuangyi
+let shuffle = (arr) => {
+  let ret = arr.slice()
+  for (let i = 0; i < ret.length; i++) {
+    let j = getRandomInt(0, i)
+    let t = ret[i]
+    ret[i] = ret[j]
+    ret[j] = t
+  }
+  return ret
+}
+
+function getSisterGenerator() {
+  const source = SISTERS.slice()
+  let initial = true
+  return () => {
+    if (initial) {
+      initial = false
+      return source.splice(0, initialSize)
+    }
+    const size = getRandomInt(0, randomSize.length - 1)
+    return source.splice(0, randomSize[size])
+  }
+}
